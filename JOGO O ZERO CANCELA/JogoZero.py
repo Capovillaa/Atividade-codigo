b65691503f34c343fb0e7d9b1932b4a8cd3cca8a
Capovillaa <gabriel.ac13@puccampinas.edu.br>
2024-04-01 19:55:48 -0300

    JogoZero
    Esse ta funcionando para quando for negativo e mensagem de 3 zeros consecutivos e soma total

---
+++ JOGO O ZERO CANCELA/JogoZero.py
@@ -0,0 +1,13 @@
+n=0
+cont_0=0
+soma=0
+while(n >= 0):
+    n=int(input("Numero:"))
+    if (n < 0):
+        break
+    elif(n == 0):
+        cont_0 += 1
+        if(cont_0 > 3):
+            print(f"Só é permitido até 3 números consecutivos!!!")
+    soma +=n
+print(f"Soma total={soma}")
