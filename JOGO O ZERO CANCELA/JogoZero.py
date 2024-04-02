4479d495ff845c44ff23ac6d600e303ae88f10e3
Capovillaa <gabriel.ac13@puccampinas.edu.br>
2024-04-02 13:28:28 -0300

    3 zeros funcionando mama

--- JOGO O ZERO CANCELA/JogoZero.py
+++ JOGO O ZERO CANCELA/JogoZero.py
@@ -11,5 +11,8 @@ while(n >= 0):
             print(f"Só é permitido até 3 números consecutivos!!!")
             if(cont_0>=3):
                 cont_0=0
+                n=int(input("Numero:"))
+                if(n==0):
+                    print(f"Só é permitido até 3 números consecutivos!!!")           
     soma +=n
 print(f"Soma total={soma}")
