b9a2ed67f6ebbd447b44722363ab787f450f3e8b
HenriqueMartins2502 <160790963+HenriqueMartins2502@users.noreply.github.com>
2024-04-12 23:35:50 -0300

    CÓDIGO FINALIZADO

--- JOGO O ZERO CANCELA/tempCodeRunnerFile.py
+++ JOGO O ZERO CANCELA/tempCodeRunnerFile.py
@@ -17,19 +17,22 @@ while n >= 0:
             zeros_consecutivos += 1
             if zeros_consecutivos == 1:
                 soma -= ultimo_numero
+                ultimo_numero=penultimo_numero
+                penultimo_numero=antepenultimo_numero
             elif zeros_consecutivos == 2:
-                soma -= penultimo_numero
+                soma -= ultimo_numero
+                ultimo_numero=penultimo_numero
             elif zeros_consecutivos == 3:
-                soma -= antepenultimo_numero
+                soma -= ultimo_numero
+            elif  zeros_consecutivos > 3:
+                cont_desconsiderados-=1
+                print(f"Só é permitido até 3 números consecutivos!!!")
     else:
         soma += numero
         antepenultimo_numero = penultimo_numero
         penultimo_numero = ultimo_numero
         ultimo_numero = numero
         zeros_consecutivos = 0
-    if  zeros_consecutivos > 3:
-        cont_desconsiderados-=1
-        print(f"Só é permitido até 3 números consecutivos!!!")
 print(f"A soma dos números é {soma}")
 print(f"Os numeros considerados são {cont_considerados - cont_desconsiderados}")
 print(f"Os numeros desconsiderados são {cont_desconsiderados}")
